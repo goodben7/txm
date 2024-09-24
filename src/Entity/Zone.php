de949d4819e304f0102e2964b2d8fddd98fbec45
Benjamin KALOMBO MUKENA <bmukena85@gmail.com>
2024-09-24 12:32:22 +0100

    [Security] add Get and GetCollection in zone entity and generate migration

--- src/Entity/Zone.php
+++ src/Entity/Zone.php
@@ -2,12 +2,31 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\Get;
 use App\Doctrine\IdGenerator;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\ZoneRepository;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Doctrine\Orm\State\ItemProvider;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
 
 #[ORM\Entity(repositoryClass: ZoneRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => 'zone:get'],
+    operations: [
+        new Get(
+            security: 'is_granted("ROLE_ZONE_DETAILS")',
+            provider: ItemProvider::class
+        ),
+        new GetCollection(
+            security: 'is_granted("ROLE_ZONE_LIST")',
+            provider: CollectionProvider::class
+        )
+    ]
+)]
 class Zone
 {
     const ID_PREFIX = "ZO";
@@ -16,16 +35,20 @@ class Zone
     #[ORM\GeneratedValue( strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(IdGenerator::class)]
     #[ORM\Column(length: 16)]
+    #[Groups(groups: ['zone:get'])]
     private ?string $id = null;
 
     #[ORM\Column(length: 30)]
+    #[Groups(groups: ['zone:get'])]
     private ?string $label = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(groups: ['zone:get'])]
     private ?string $description = null;
 
     #[ORM\Column]
-    private ?bool $actived = null;
+    #[Groups(groups: ['zone:get'])]
+    private ?bool $actived = true;
 
     public function getId(): ?string
     {
