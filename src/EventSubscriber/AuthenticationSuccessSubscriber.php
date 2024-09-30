26d46a0859fd6837ff15cf8f04b97974bf874216
Benjamin KALOMBO MUKENA <bmukena85@gmail.com>
2024-09-30 15:47:56 +0100

    [Security] block user connection if deleted

---
+++ src/EventSubscriber/AuthenticationSuccessSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\SecurityBundle\Security;
+use App\Exception\UserAuthenticationException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+
+class AuthenticationSuccessSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private Security $security,
+        private UserRepository $userRepository
+    )
+    {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'lexik_jwt_authentication.on_authentication_success' => 'onAuthenticationSuccess',
+        ];
+    }
+
+    public function onAuthenticationSuccess(AuthenticationSuccessEvent $event)
+    {
+        $email = $this->security->getUser()->getUserIdentifier();
+
+        /** @var User|null $user */
+        $user = $this->userRepository->findOneBy(['email' => $email]);
+
+
+        if ($user->isDeleted()) {
+            
+            throw new UserAuthenticationException('This user is not active. Please contact support.');
+        }
+    }
+}
