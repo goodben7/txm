cf2c4193cc5f73aa3f9e8f6ef2e574fee1ef2fd7
bmukena85 <bmukena85@gmail.com>
2025-08-19 16:01:43 +0100

    [Delivery Service] add img in products - feat(product): update Product entity and add new migration

---
+++ migrations/Version20250819144557.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250819144557 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product ADD file_path VARCHAR(255) DEFAULT NULL, ADD file_size INT DEFAULT NULL, ADD file_path_secondary VARCHAR(255) DEFAULT NULL, ADD file_size_secondary INT DEFAULT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product DROP file_path, DROP file_size, DROP file_path_secondary, DROP file_size_secondary
+        SQL);
+    }
+}
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -22,8 +22,11 @@ use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use Symfony\Component\Serializer\Annotation\Groups;
 use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
 use ApiPlatform\Doctrine\Common\State\PersistProcessor;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
+#[Vich\Uploadable]
 #[ORM\HasLifecycleCallbacks]
 #[ApiResource(
     normalizationContext: ['groups' => 'product:get'], 
@@ -46,6 +49,14 @@ use ApiPlatform\Doctrine\Common\State\PersistProcessor;
             denormalizationContext: ['groups' => 'product:patch'],
             processor: PersistProcessor::class,
         ),
+        new Post(
+            uriTemplate: "products/{id}/logo",
+            denormalizationContext: ['groups' => 'product:logo'],
+            security: 'is_granted("ROLE_PRODUCT_UPDATE")',
+            inputFormats: ['multipart' => ['multipart/form-data']],
+            processor: PersistProcessor::class,
+            status: 200
+        )
     ]
 )]
 #[ApiFilter(SearchFilter::class, properties: [
@@ -100,6 +111,36 @@ class Product implements RessourceInterface
     #[ORM\Column(nullable: true)]
     #[Groups(['product:get'])]
     private ?\DateTimeImmutable $updatedAt = null;
+
+    #[Groups(groups: ['product:logo'])]
+    #[Vich\UploadableField(mapping: 'media_object', fileNameProperty: 'filePath', size: 'fileSize')]
+    private ?File $file = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(groups: ['product:get'])]
+    private ?string $filePath = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Groups(groups: ['product:get'])]
+    private ?int $fileSize = null;
+
+    #[Groups(groups: ['product:get'])]
+    private ?string $contentUrl;
+
+    #[Groups(groups: ['product:logo'])]
+    #[Vich\UploadableField(mapping: 'media_object', fileNameProperty: 'filePathSecondary', size: 'fileSizeSecondary')]
+    private ?File $fileSecondary = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(groups: ['product:get'])]
+    private ?string $filePathSecondary = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Groups(groups: ['product:get'])]
+    private ?int $fileSizeSecondary = null;
+
+    #[Groups(groups: ['product:get'])]
+    private ?string $contentUrlSecondary;
 
     public function getId(): ?string
     {
@@ -206,5 +247,157 @@ class Product implements RessourceInterface
     public function updateUpdatedAt(): void
     {
         $this->updatedAt = new \DateTimeImmutable();
+    }
+
+    /**
+     * Get the value of filePath
+     */ 
+    public function getFilePath(): string|null
+    {
+        return $this->filePath;
+    }
+
+    /**
+     * Set the value of filePath
+     *
+     * @return  self
+     */ 
+    public function setFilePath(?string $filePath): static
+    {
+        $this->filePath = $filePath;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of fileSize
+     */ 
+    public function getFileSize(): int|null
+    {
+        return $this->fileSize;
+    }
+
+    /**
+     * Set the value of fileSize
+     *
+     * @return  self
+     */ 
+    public function setFileSize(?int $fileSize): static
+    {
+        $this->fileSize = $fileSize;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of file
+     */ 
+    public function getFile(): File|null
+    {
+        return $this->file;
+    }
+
+    /**
+     * Set the value of file
+     *
+     * @return  self
+     */ 
+    public function setFile($file): static
+    {
+        $this->file = $file;
+
+        if (null !== $file) {
+            $this->updatedAt = new \DateTimeImmutable('now');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the value of contentUrl
+     */ 
+    public function getContentUrl(): string|null
+    {
+        return $this->contentUrl;
+    }
+
+    /**
+     * Set the value of contentUrl
+     *
+     * @return  self
+     */ 
+    public function setContentUrl($contentUrl): static
+    {
+        $this->contentUrl = $contentUrl;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of file
+     */ 
+    public function getFileSecondary()
+    {
+        return $this->fileSecondary;
+    }
+
+    /**
+     * Set the value of file
+     *
+     * @return  self
+     */ 
+    public function setFileSecondary($fileSecondary)
+    {
+        $this->fileSecondary = $fileSecondary;
+
+        if (null !== $fileSecondary) {
+            $this->updatedAt = new \DateTimeImmutable('now');
+        }
+
+        return $this;
+    }
+
+    public function getFilePathSecondary(): ?string
+    {
+        return $this->filePathSecondary;
+    }
+
+    public function setFilePathSecondary(?string $filePathSecondary): self
+    {
+        $this->filePathSecondary = $filePathSecondary;
+
+        return $this;
+    }
+
+    public function getFileSizeSecondary(): ?int
+    {
+        return $this->fileSizeSecondary;
+    }
+
+    public function setFileSizeSecondary(?int $fileSizeSecondary): self
+    {
+        $this->fileSizeSecondary = $fileSizeSecondary;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of contentUrlSecondary
+     */ 
+    public function getContentUrlSecondary(): ?string
+    {
+        return $this->contentUrlSecondary;
+    }
+
+    /**
+     * Set the value of contentUrlSecondary
+     *
+     * @return  self
+     */ 
+    public function setContentUrlSecondary(?string $contentUrlSecondary): self
+    {
+        $this->contentUrlSecondary = $contentUrlSecondary;
+
+        return $this;
     }
 }
