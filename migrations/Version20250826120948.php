f02043671d180d38015baaf273e7c918dffd029a
bmukena85 <bmukena85@gmail.com>
2025-08-26 13:40:03 +0100

    fix(migration): update Version20250826120948 migration

--- migrations/Version20250826120948.php
+++ migrations/Version20250826120948.php
@@ -21,6 +21,9 @@ final class Version20250826120948 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
+            ALTER TABLE product ADD type_id INT DEFAULT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
             ALTER TABLE product ADD CONSTRAINT FK_D34A04ADC54C8C93 FOREIGN KEY (type_id) REFERENCES product_type (id)
         SQL);
         $this->addSql(<<<'SQL'
@@ -36,6 +39,9 @@ final class Version20250826120948 extends AbstractMigration
         SQL);
         $this->addSql(<<<'SQL'
             DROP INDEX IDX_D34A04ADC54C8C93 ON product
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product DROP type_id
         SQL);
     }
 }
